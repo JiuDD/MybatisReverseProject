7006d51bcb8ea54ccd545757e75e691a12e0d9d1
jiudongdongdd01 <jiudongdong@300.cn>
2021-04-23 16:52:03 +0800

    init

---
+++ src/main/java/GeneratorSqlmapReport.java
@@ -0,0 +1,40 @@
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * description: 使用MyBatis逆向生成pojo、dao、mapper文件。此模板可以正确生成。
+ * @author: JiuDongDong
+ * date: 2020/5/12.
+ */
+public class GeneratorSqlmapReport {
+
+    public void generator() throws Exception{
+        List<String> warnings = new ArrayList<>();
+        boolean overwrite = true;
+        //指定 逆向工程配置文件
+        File configFile = new File("E:\\Tedu\\MyAchieveFiles\\MybatisReverseProject\\src\\main\\resources\\generatorReport.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+                callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+
+    //生成逆向文件
+    public static void main(String[] args) throws Exception {
+        try {
+            GeneratorSqlmapReport generatorSqlmap = new GeneratorSqlmapReport();
+            generatorSqlmap.generator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
