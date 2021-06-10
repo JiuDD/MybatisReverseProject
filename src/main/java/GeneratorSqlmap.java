57db9303b69518545eacddc4f810455e47dec9c2
jiudongdongdd01 <jiudongdong@300.cn>
2021-06-10 20:37:15 +0800

    pudate

--- src/main/java/GeneratorSqlmap.java
+++ src/main/java/GeneratorSqlmap.java
@@ -1,9 +1,12 @@
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.XMLParserException;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,28 +16,36 @@ import java.util.List;
  * date: 2018/5/29.
  */
 public class GeneratorSqlmap {
-
-    public void generator() throws Exception{
+    /**
+     * Description: //生成逆向文件
+     * @author: JiuDD
+     * date: 2021/6/10 20:15
+     */
+    public static void main(String[] args) throws Exception {
+        //MBG 执行过程中的警告信息
         List<String> warnings = new ArrayList<>();
+        //当生成的代码重复时，覆盖原代码
         boolean overwrite = true;
-        //指定 逆向工程配置文件
+        //读取我们的 MBG 配置文件 逆向工程配置文件
         File configFile = new File("src\\main\\resources\\generator.xml");
         ConfigurationParser cp = new ConfigurationParser(warnings);
         Configuration config = cp.parseConfiguration(configFile);
+
+//        这4行代码作用也是读取配置文件并解析
+//        InputStream is = GeneratorSqlmap.class.getResourceAsStream("/generator.xml");
+//        ConfigurationParser cp = new ConfigurationParser(warnings);
+//        Configuration config = cp.parseConfiguration(is);
+//        is.close();
+
         DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
-                callback, warnings);
+        //创建 MBG
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        //执行生成代码
         myBatisGenerator.generate(null);
-    }
-
-    //生成逆向文件
-    public static void main(String[] args) throws Exception {
-        try {
-            GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
-            generatorSqlmap.generator();
-        } catch (Exception e) {
-            e.printStackTrace();
+        //输出警告信息
+        for (String warning : warnings) {
+            System.out.println(warning);
         }
-
     }
+
 }
