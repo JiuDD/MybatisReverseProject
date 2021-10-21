fe52bd1d83fe965edeaaf56244181bc947f2d807
JiuDD <jiudd0424@163.com>
2021-10-21 15:59:55 +0800

    up

--- src/main/java/com/macro/mall/mapper/ViewDetailMapper.java
+++
@@ -1,9 +0,0 @@
-package com.macro.mall.mapper;
-
-import com.macro.mall.model.ViewDetail;
-
-public interface ViewDetailMapper {
-    int insert(ViewDetail record);
-
-    int insertSelective(ViewDetail record);
-}
